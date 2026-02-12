5ae411879b90e7666a6e93825bb3c1701040a2c5
Sanketh Kodimala <kodim003@umn.edu>
2026-02-11 20:39:55 -0600

    Inital commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,60 @@
+<script>
+    let maxClick = $state(6); //Setting the default to 6
+    let cnt = $state(maxClick); // tip: https://svelte.dev/docs/svelte/$state
+    
+  
+    function onClick() {
+      // tip: Since DOM (i.e., the webpage content) will automatically update based on values, [<p id="info">Remaining Number of Clicks: {cnt}</p>]
+      // we only need to change the cnt number here. 
+      if (cnt > 0) {
+        cnt = cnt -1;
+      } 
+    }
+
+  </script>
+  
+  <h1>Sanketh's VIS Site</h1>
+  <img
+    width="200px"
+    src="https://media.istockphoto.com/id/523761634/photo/cute-panda-bear-climbing-in-tree.jpg?s=612x612&w=0&k=20&c=TxsmORsbuY1LpxQsc6T8fpWJo7lBwncciYhroAr8rXI="
+  />
+  <div>
+    You can click up to
+    <select 
+        bind:value={maxClick} 
+        onchange={() => (cnt = maxClick)}>
+      {#each [2, 4, 6] as optionNum}
+        <option value={optionNum}>
+          {optionNum}
+        </option>
+      {/each}
+    </select>
+    times
+  </div>
+  <button onclick={onClick}> Click Me </button>
+
+  <!-- tip: use {#if...} template syntax here (https://svelte.dev/docs/svelte/if) so that the content below will automatically update when cnt value changes -->
+    <!-- `the content below should only show when cnt >0` -->
+{#if cnt>0}
+    <p id="info">Remaining Number of Clicks: {cnt}</p>
+    <!-- The content below should only shown when cnt =0 -->
+{:else}
+    <p>No more clicks allowed</p>
+{/if}
+  
+  <style>
+    body {
+      font-family: Arial, Helvetica, sans-serif;
+    }
+    button {
+      background-color: #44aa66;
+      /* background-color: blue; */
+      color: white;
+      font-size: xx-large;
+      padding: 10px 20px;
+      border: none;
+      cursor: pointer;
+      border-radius: 5px;
+    }
+  </style>
+  
